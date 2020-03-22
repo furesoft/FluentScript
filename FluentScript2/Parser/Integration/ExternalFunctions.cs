1ddd9a11525ebbbfea77c271945d39576be696ac
Furesoft <filmee24@gmail.com>
2020-03-22 09:55:47 +0100

    Initial Commit

---
+++ FluentScript2/Parser/Integration/ExternalFunctions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+// <lang:using>
+using ComLib.Lang.Core;
+using ComLib.Lang.AST;
+using ComLib.Lang.Types;
+using ComLib.Lang.Helpers;
+// </lang:using>
+
+
+namespace ComLib.Lang.Parsing
+{
+    /// <summary>
+    /// Helper class for calling functions
+    /// </summary>
+    public class ExternalFunctions
+    {
+        private Dictionary<string, Func<string, string, FunctionCallExpr, object>> _customCallbacks;
+        private Dictionary<string, string> _lcaseToFormaNameMap;
+
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        public ExternalFunctions()
+        {
+            _customCallbacks = new Dictionary<string, Func<string, string, FunctionCallExpr, object>>();
+            _lcaseToFormaNameMap = new Dictionary<string, string>();
+        }
+
+
+        /// <summary>
+        /// Registers a custom function callback.
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="callback">The custom callback</param>
+        public void Register(string pattern, Func<string, string, FunctionCallExpr, object> callback)
+        {
+            _customCallbacks[pattern] = callback;
+            _lcaseToFormaNameMap[pattern.ToLower()] = pattern;
+        }
+
+        
+        /// <summary>
+        /// Whether or not the function call supplied is a custom function callback that is 
+        /// outside of the script.
+        /// </summary>
+        /// <param name="name">Name of the function</param>
+        /// <returns></returns>
+        public bool Contains(string name)
+        {
+            var callback = GetByName(name);
+            return callback != null;
+        }
+
+
+        /// <summary>
+        /// Get the formal case sensitive function name that matches the case insensitive function name supplied.
+        /// </summary>
+        /// <param name="name">Name of the function</param>
+        /// <returns></returns>
+        public string GetMatch(string name)
+        {
+            if (!_lcaseToFormaNameMap.ContainsKey(name))
+                return null;
+            return _lcaseToFormaNameMap[name];
+        }
+
+
+        /// <summary>
+        /// Get the custom function callback
+        /// </summary>
+        /// <param name="name">Name of the function</param>
+        /// <returns></returns>
+        public Func<string, string, FunctionCallExpr, object> GetByName(string name)
+        {
+            // Contains callback for full function name ? e.g. CreateUser
+            if (_customCallbacks.ContainsKey(name))
+                return _customCallbacks[name];
+                    
+
+            // Contains callback that handles multiple methods on a "object".
+            // e.g. Blog.Create, Blog.Delete etc.
+            if (name.Contains("."))
+            {
+                var prefix = name.Substring(0, name.IndexOf("."));
+                if (_customCallbacks.ContainsKey(prefix + ".*"))
+                    return _customCallbacks[prefix + ".*"];
+            }
+            return null;
+        }   
+    }
+}
---
+++ Terminal/Program.cs
@@ -0,0 +1,31 @@
+﻿using ComLib.Lang;
+using ComLib.Lang.Core;
+using ComLib.Lang.Plugins;
+using System;
+
+namespace Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = new Interpreter();
+            i.InitPlugins();
+            i.Context.Plugins.RegisterAll();
+            i.Settings.EnablePrinting = true;
+            i.Settings.EnableFluentMode = true;
+            i.Settings.EnableFunctionCallCallBacks = true;
+
+            while (true)
+            {
+                var src = Console.ReadLine();
+
+                i.AppendExecute(src);
+
+                if (!i.Result.Success)
+                    Console.WriteLine(i.Result);
+            }
+
+        }
+    }
+}
